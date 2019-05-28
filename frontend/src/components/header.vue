91c13f082c64920306fe9c4f19cf74ec892a54a4
Liren He <hlr12138@gmail.com>
2019-05-28 21:35:05 +0800

    optimize the header

--- frontend/src/components/header.vue
+++ frontend/src/components/header.vue
@@ -1,24 +1,30 @@
 <template>
 <div class="header">
   <el-row :gutter="20" type="flex" justify="center">
-    <el-col :span="5" class="logo" @click="homeBtn">MyBlog</el-col>
+    <el-col :span="5" class="logo" @click="homeBtn">
+      <i class="el-icon-watermelon"></i>MyBlog</el-col>
     <el-col :span="14">
-      <el-row :gutter="20" type="flex" justify="start" class="menu">
-        <el-col :span="4">
-          <div :class="activeIndex == 1 ? 'active' : ''" @click="homeBtn">首页</div>
+      <el-row :gutter="3" type="flex" justify="start" class="menu">
+        <el-col :span="3">
+          <div :class="activeIndex == 1 ? 'active' : ''" @click="homeBtn">
+            <i class="el-icon-s-home"></i>首页</div>
+        </el-col>
+        <el-col :span="3">
+          <div :class="activeIndex == 2 ? 'active' : ''" @click="classBtn">
+            <i class="el-icon-s-grid"></i>分类</div>
+        </el-col>
+        <el-col :span="3">
+          <div :class="activeIndex == 3 ? 'active' : ''" @click="aboutBtn">
+            <i class="el-icon-s-custom"></i>关于我</div>
         </el-col>
         <el-col :span="4">
-          <div :class="activeIndex == 2 ? 'active' : ''" @click="classBtn">分类</div>
-        </el-col>
-        <el-col :span="4">
-          <div :class="activeIndex == 3 ? 'active' : ''" @click="aboutBtn">关于我</div>
-        </el-col>
-        <el-col :span="4">
-          <div :class="activeIndex == 4 ? 'active' : ''" @click="startBtn">开始场景</div>
+          <div :class="activeIndex == 4 ? 'active' : ''" @click="startBtn">
+            <i class="el-icon-s-promotion"></i>开始场景</div>
         </el-col>
       </el-row>
     </el-col>
-    <el-col :span="5"><div class="login" @click="loginBtn">登录|注册</div></el-col>
+    <el-col :span="4"><div class="login" @click="loginBtn">
+      <i class="el-icon-user-solid"></i>登录|注册</div></el-col>
   </el-row>
 </div>
 </template>
@@ -34,16 +40,24 @@
   color: white;
   height: 50px;
   line-height: 50px;
-  min-width: 900px;
+  min-width: 750px;
+	font-family: Arial, sans-serif;
+  font-weight: 700;
+  /* forbid select text */
+  -moz-user-select: none; 
+  -o-user-select:none; 
+  -khtml-user-select:none; 
+  -webkit-user-select:none; 
+  -ms-user-select:none; 
+  user-select:none;
 }
 
 .logo {
   font-size: 30px;
-  font-family: 'Microsoft YaHei';
   cursor: pointer;
 }
 
-.menu .el-col:hover,
+.menu .el-col:hover div,
 .menu .el-col:hover a,
 .active {
   background-color: #48456C;
@@ -51,7 +65,6 @@
 }
 
 .menu .el-col {
-  margin: 0 5px;
   cursor: pointer;
 }
 
@@ -93,7 +106,7 @@ export default {
       });
     },
     startBtn() {
-      window.location.href = 'http://localhost/ocean'
+      window.location.href = 'http://localhost/start'
     },
     loginBtn() {
       this.$router.push ({
